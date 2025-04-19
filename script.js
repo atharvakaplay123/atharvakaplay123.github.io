7aabf33dac85fc387b0efff37d7b43431550a80e
atharvakaplay123 <atharvakaplay@gmail.com>
2025-04-19 18:36:55 +0530

    Bug Fixes(navbar scroll shrink animation bug fixed)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', () => {
     const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 10) {
+    if (window.scrollY >= 70) {
       navbar.classList.add('shrink');
     } else {
       navbar.classList.remove('shrink');
