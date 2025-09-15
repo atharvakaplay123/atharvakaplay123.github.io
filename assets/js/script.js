060b178cc4009cf93346aa156f113ab32251555f
atharvakaplay123 <atharvakaplay@gmail.com>
2025-09-15 09:18:54 +0530

    added skill classification tabs buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,68 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('shrink');
   }
 });
+function all_skills() {
+  no_skills_btn();
+  document.getElementById("all_skills").style.color = "#ffffff"
+  document.getElementById("all_skills").style.backgroundColor = "#0097b2"
+  Array.from(document.getElementById("skills-grid").children).forEach((e) => {
+    e.style.display = "unset";
+  })
+}
+document.addEventListener('DOMContentLoaded', all_skills);
+function no_skills() {
+  Array.from(document.getElementById("skills-grid").children).forEach((e) => {
+    e.style.display = "none";
+  })
+}
+function no_skills_btn() {
+  Array.from(document.getElementById("skill_btns").children).forEach((e) => {
+    e.style.backgroundColor = "#000000"
+    e.style.color = "#0097b2"
+  })
+}
+function web_skills() {
+  no_skills_btn();
+  document.getElementById("web_skills").style.color = "#ffffff"
+  document.getElementById("web_skills").style.backgroundColor = "#0097b2"
+  no_skills();
+  Array.from(document.getElementsByClassName("web-dev")).forEach((e) => {
+    e.style.display = "unset";
+  })
+}
+function lang_skills() {
+  no_skills_btn();
+  document.getElementById("lang_skills").style.color = "#ffffff"
+  document.getElementById("lang_skills").style.backgroundColor = "#0097b2"
+  no_skills();
+  Array.from(document.getElementsByClassName("language")).forEach((e) => {
+    e.style.display = "unset";
+  })
+}
+function tools_skills() {
+  no_skills_btn();
+  document.getElementById("tools_skills").style.color = "#ffffff"
+  document.getElementById("tools_skills").style.backgroundColor = "#0097b2"
+  no_skills();
+  Array.from(document.getElementsByClassName("tools")).forEach((e) => {
+    e.style.display = "unset";
+  })
+}
+function db_skills() {
+  no_skills_btn();
+  document.getElementById("db_skills").style.color = "#ffffff"
+  document.getElementById("db_skills").style.backgroundColor = "#0097b2"
+  no_skills();
+  Array.from(document.getElementsByClassName("DB")).forEach((e) => {
+    e.style.display = "unset";
+  })
+}
+function robotics_skills() {
+  no_skills_btn();
+  document.getElementById("robotics_skills").style.color = "#ffffff"
+  document.getElementById("robotics_skills").style.backgroundColor = "#0097b2"
+  no_skills();
+  Array.from(document.getElementsByClassName("robotics")).forEach((e) => {
+    e.style.display = "unset";
+  })
+}
